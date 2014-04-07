a268322f4544ec4e650c414550acba8642e88991
Manish Kumar <manish.kumar5@tcs.com>
2014-04-07 09:57:03 +0530

    Initial repository

---
+++ Gemfile
@@ -0,0 +1,48 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '3.2.15'
+gem 'pg', '0.15.1'
+gem 'sqlite3'
+
+group :development, :test do
+  #gem 'sqlite3'
+  gem 'rspec-rails'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
+  #gem 'linecache19', '0.5.13'
+  #gem 'ruby-debug-base19', '0.11.26'
+  #gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :test do
+  gem "mocha", :require => false
+  gem 'test_after_commit'
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
+end
+#gem 'capybara', '2.1.0'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets'
+gem 'factory_girl'
+gem 'strong_parameters'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
+gem 'bcrypt-ruby', '~> 3.0.1'
+# group :doc do
+#   gem 'sdoc', '0.3.20', require: false
+# end
+
+group :production do
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,33 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+ChirpersApp::Application.config.secret_key_base = secure_token
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChirpersApp::Application.config.secret_token = '51a31215be41c73f410c58d7f80d1e73d0d91ceda9007aa1b3e6a0f319479bd52daa5da1897fda1ad8c1eabbc7dddbd4788d802109494aa3459391ef38071fbb'
