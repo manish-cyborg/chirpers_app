77940c48fd2b5483cccd7f41b47f9024cee659cc
Manish Kumar <manish.kumar5@tcs.com>
2014-04-10 09:05:40 +0530

    Finished layouts and commits

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 ChirpersApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  get "users/new"
 
-  get "static_pages/about"
+  root  to: 'static_pages#home'
 
-  get "static_pages/contact"
+  match '/signup',  to: 'users#new', via: 'get'
+  match '/home', to: 'static_pages#home', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
